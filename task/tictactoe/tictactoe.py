2ac2d3a0167c4fa0b6353cf9d09bf137e386488e
Sergey Gurman <sgurman753@gmail.com>
2020-06-02 18:13:36 -0400

    stage 2/5: added commits

--- task/tictactoe/tictactoe.py
+++ task/tictactoe/tictactoe.py
@@ -4,8 +4,8 @@ def frame():
 
 
 cell = str(input("Enter cells: "))
-cell = [" "+i for i in cell]
-cell = [cell[i:i+3] for i in range(0, len(cell), 3)]
+cell = [" "+i for i in cell]  # List with one "front space" with a "number"
+cell = [cell[i:i+3] for i in range(0, len(cell), 3)]  # 3 values in a list within a list
 
 frame()
 for x in range(0, len(cell)):
