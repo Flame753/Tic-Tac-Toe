ee55c2272cffe3aa34636e0a52851007e3be3df4
Sergey Gurman <sgurman753@gmail.com>
2020-06-02 18:03:37 -0400

    Stage 2/5

--- task/tictactoe/tictactoe.py
+++ task/tictactoe/tictactoe.py
@@ -1,2 +1,13 @@
 # write your code here
-print("X O X\nO X O\nX X O")
+def frame():
+    print("---------")
+
+
+cell = str(input("Enter cells: "))
+cell = [" "+i for i in cell]
+cell = [cell[i:i+3] for i in range(0, len(cell), 3)]
+
+frame()
+for x in range(0, len(cell)):
+    print(f"|{''.join(cell[x])} |")
+frame()
