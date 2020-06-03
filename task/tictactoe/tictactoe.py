6d6efaaace0667f8b98635adcdae370a51a324d0
Sergey Gurman <sgurman753@gmail.com>
2020-06-03 19:09:55 -0400

    finished is_three_row function

--- task/tictactoe/tictactoe.py
+++ task/tictactoe/tictactoe.py
@@ -2,23 +2,37 @@ def frame():
     print("---------")
 
 
-def reset(task):
-    task = 0
-    return task
+def winner(who):
+    return f"{who} wins"
 
 
 def is_three_row(board, p):
     symbol = " " + p
-    count = 0
+    count_col = 0
+    count_diag1 = 0
+    count_diag2 = 0
+    row_decrease = len(board) - 1
     for row in range(len(board)):
         if board[row].count(symbol) == 3:
-            pass  # Player won
+            return True, winner(symbol)  # Player won by a row
+        if board[row][row] == symbol:
+            count_diag1 += 1
+            if count_diag1 == 3:
+                return True, winner(symbol)  # Player won by a diagonal \
+        if board[row_decrease][row_decrease] == symbol:
+            count_diag2 += 1
+            if count_diag2 == 3:
+                return True, winner(symbol)  # Player won by a diagonal /
         for col in range(len(board[row])):
             if board[col][row] == symbol:
-                count += 1
-                if count == 3:
-                    pass  # Player won
-        reset()
+                count_col += 1
+                if count_col == 3:
+                    return True, winner(symbol)  # Player won by a column
+        row_decrease -= 1
+        count_col = 0
+        count_diag1 = 0
+        count_diag2 = 0
+    return False, ""
 
 
 cell = str(input("Enter cells: "))
@@ -31,4 +45,5 @@ for x in range(0, len(cell)):
 frame()
 
 print(cell)
-is_three_row(cell, "x", "o")
+print(is_three_row(cell, "x")[1])
+print(is_three_row(cell, "o")[0])
